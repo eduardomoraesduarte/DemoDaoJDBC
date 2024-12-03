6c752987384307cd33d3a69b75fd839d508842e9
Eduardo Moraes Duarte <eduardomoraesduarte@gmail.com>
2024-12-03 16:08:12 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
@@ -11,6 +13,8 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(obj);
 		System.out.println(seller);
